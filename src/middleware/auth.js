3e2840ddb81b3a7149aeb0bf1430e88be4946990
akhileshasapu <156326254+akhileshasapu@users.noreply.github.com>
2025-06-20 22:15:45 +0530

    Update auth.js

--- src/middleware/auth.js
+++ src/middleware/auth.js
@@ -1,28 +1,34 @@
-require("dotenv").config();
-const jwt = require("jsonwebtoken")
-const JWT_SECRET=process.env.JWT_SECRET ;
+import dotenv from "dotenv";
+import jwt from "jsonwebtoken";
+
+dotenv.config();
+
+const JWT_SECRET = process.env.JWT_SECRET;
+
 if (!JWT_SECRET) {
-  console.error("❌ JWT_SECRET not loaded from .env");
+  console.error(" JWT_SECRET not loaded from .env");
   process.exit(1);
 }
 
-function auth(req,res,next){
- const authHeader = req.headers["authorization"]
- const token = authHeader && authHeader.split(" ")[1];
- console.log("Token recvied:", req.headers["authorization"]);
- console.log("JWT_SECRET IS:",JWT_SECRET)
+function auth(req, res, next) {
+  const authHeader = req.headers["authorization"];
+  const token = authHeader && authHeader.split(" ")[1];
 
- if(!token)
- {
-    return res.status(401).json({message:"access denied :no token"})
- }
- try{
-    const decoded = jwt.verify(token,JWT_SECRET);
-    req.user = decoded // { id: "userId123", email: "email@example.com" }
+  console.log("Token received:", req.headers["authorization"]);
+  console.log("JWT_SECRET is:", JWT_SECRET);
+
+  if (!token) {
+    return res.status(401).json({ message: "Access Denied: No token" });
+  }
+
+  try {
+    const decoded = jwt.verify(token, JWT_SECRET);
+    req.user = decoded; // e.g., { _id, email, username }
     next();
- }catch(err){
-   console.log("the error is:",err)
+  } catch (err) {
+    console.log("The error is:", err);
     return res.status(403).json({ message: "Access Denied: Invalid token" });
- }
+  }
 }
-module.exports = auth
+
+export default auth;
