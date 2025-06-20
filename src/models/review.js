7b61680c159e5f94d2af61bee2cb1162d2f32fea
akhileshasapu <156326254+akhileshasapu@users.noreply.github.com>
2025-06-20 22:14:31 +0530

    Update review.js

--- src/models/review.js
+++ src/models/review.js
@@ -1,9 +1,14 @@
-const mongoose = require("mongoose")
+import mongoose from "mongoose";
 
-const ReviewSchema = new mongoose.Schema({
-movieId:{type:String,required:true},
-userId:{type:mongoose.Schema.Types.ObjectId,ref:"user"},
-reviewText:{type:String,required:true},
-score:{type:Number,required:true,timestamps: true }
-})
-module.exports = mongoose.model("review",ReviewSchema)
+const ReviewSchema = new mongoose.Schema(
+  {
+    movieId: { type: String, required: true },
+    userId: { type: mongoose.Schema.Types.ObjectId, ref: "user" },
+    reviewText: { type: String, required: true },
+    score: { type: Number, required: true }
+  },
+  { timestamps: true } // Correct placement for timestamps
+);
+
+const Review = mongoose.model("review", ReviewSchema);
+export default Review;
